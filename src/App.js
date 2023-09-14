4382f084c97d7225477c67834a45ad6466b3c490
Subharup Chakraborty <82225359+ugoslav@users.noreply.github.com>
2023-09-14 21:44:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const InputArea = (props) => {
+    return(
+        <div>
+            <form onSubmit={props.submissionHandler}>
+                <input placeholder='write your note here...' value={props.noteText} onChange={(e) => props.setNoteText(e.target.value)}></input>
+                <button type='submit'>submit</button>
+            </form>
+        </div>
+    )
 }
 
-export default App;
+const DisplayArea = (props) => {
+    const notes = props.notes
+    return(
+        <div>
+            <ol>
+                {notes.map(item => {
+                    return (
+                        <div key={item}>
+                            <h2>  <li>{item} <button style={{marginLeft: "10px"}} onClick={() => props.deleteHandler(item)}>delete</button></li>  </h2>
+                        </div>
+                )
+                })}
+            </ol>
+        </div>
+    )
+}
+
+const App = () => {
+    const [todoList , setTodoList] = useState([])
+    const [noteText , setNoteText] = useState('')
+
+    const submissionHandler = (e) => {
+        e.preventDefault()
+        setTodoList(todoList.concat(noteText))
+        setNoteText('')
+    }
+
+    const deleteHandler = (target) => {
+        let filteredArray = todoList.filter(item => item !== target)
+        setTodoList(filteredArray)
+    }
+
+  return (
+    <div>
+      <InputArea noteText={noteText} setNoteText={setNoteText} submissionHandler={submissionHandler}/>
+      <DisplayArea notes={todoList} deleteHandler={deleteHandler}/>
+    </div>
+  )
+}
+
+export default App
